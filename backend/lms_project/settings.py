38dc64a8808ed8ec3831b956d4fcaeeb654c080a
abrarfahimabir <abrarfahim448@gmail.com>
2026-05-04 03:21:19 +0600

    postgree db added

--- backend/lms_project/settings.py
+++ backend/lms_project/settings.py
@@ -108,11 +108,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lms_project.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
